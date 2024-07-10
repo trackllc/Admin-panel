d1cfd675151d3c4e8b0131c74290c748a64723c6
novakand <a79264799999@gmail.com>
2024-07-10 23:31:44 +0300

    fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,9 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
 import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
 
